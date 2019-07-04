acd2adeea09e1fcf3e125d4a6113a9a2d8c125aa
Loi Tran <loi.tran@idsolutions.com.vn>
2019-07-04 17:58:05 +0700

    Add test and update queue event mapping

--- server/belga/io/feed_parsers/belga_queue_events.py
+++ server/belga/io/feed_parsers/belga_queue_events.py
@@ -25,13 +25,13 @@ class BelgaQueueEventsParser(FeedParser):
     """Parser for events pushed from Belga Agenda through ActiveMQ
     """
 
-    NAME = 'belgaqueueevents'
+    NAME = 'belga_queue_events'
 
     label = 'Belga Queue Events Parser'
 
     REQUIRED_FIELD = [
-        'eventId', 'startDate', 'endDate', 'createDate', 'url', 'contacts', 'countryCode', 'eventUploadContents'
-        'mainRubric.rubricTexts', 'rubricEvent', 'eventUploadContents', 'recurrence',
+        'eventId', 'startDate', 'endDate', 'createdDate', 'url', 'contacts', 'countryCode', 'eventUploadContents',
+        'mainRubric.rubricTexts', 'rubricEvent', 'eventUploadContents',
         'venueInfo.venueId', 'venueInfo.street', 'venueInfo.streetNumber', 'venueInfo.country.countryTexts'
     ]
     OPTIONAL_FIELD = ['lastUpdateTime']
@@ -45,8 +45,8 @@ class BelgaQueueEventsParser(FeedParser):
     def _get(self, dict_item, keys):
         """Return nested item in dict via dot notation
 
-        >> _get({'a': {'b': 'c'}}, 'a.b') == 'c'
-        True
+        >>> _get({'a': {'b': 'c'}}, 'a.b')
+        c
         """
         item = deepcopy(dict_item)
         for key in keys.split('.'):
@@ -55,53 +55,57 @@ class BelgaQueueEventsParser(FeedParser):
                 return item
         return item
 
-    def parse(self, data):
-        timezone = country_timezones.get(data['countryCode'])
-        if not timezone:
-            raise ValueError('Invalid timezone')
-        timezone = timezone[0]  # pytz country timezones return list
+    def parse(self, data, provider=None):
+        try:
+            timezone = country_timezones.get(data['countryCode'])
+            if not timezone:
+                raise ValueError('Invalid timezone')
+            timezone = timezone[0]  # pytz country timezones are list
 
-        item = {
-            'original_id': data['eventId'],
-            'type': 'event',
-            'dates': {
-                'start': self._parse_date(timezone, data['startDate']),
-                'end': self._parse_date(timezone, data['endDate']),
-                'tz': timezone,
-            },
-            'calendars': [],
-            'locations': self._parse_location(data['venueInfo']),
-            'contacts': self._parse_contacts(data['contacts']),
-            'links': [data['url']],
-            'versioncreated': self._parse_date(timezone, data['createdDate']),
-            'firstcreated': self._parse_date(timezone, data['createdDate']),
-        }
-        event = self._get_en_item(data['eventContents'])
-        item.update({'name': event['title'], 'slugline': event['subject']})
+            item = {
+                'original_id': data['eventId'],
+                'type': 'event',
+                'dates': {
+                    'start': self._parse_date(timezone, data['startDate']),
+                    'end': self._parse_date(timezone, data['endDate']),
+                    'tz': timezone,
+                },
+                'calendars': [],
+                'locations': self._parse_location(data['venueInfo']),
+                'contacts': self._parse_contacts(data['contacts']),
+                'links': [data['url']],
+                'versioncreated': self._parse_date(timezone, data['createdDate']),
+                'firstcreated': self._parse_date(timezone, data['createdDate']),
+            }
+            event = self._get_en_item(data['eventContents'])
+            item.update({'name': event['title'], 'definition_short': event['subject']})
 
-        # update contact updated and created date:
-        for c1, c2 in zip(item['contacts'], data['contacts']):
-            c1["_updated"] = self._parse_date(timezone, c2['updatedDate'])
-            c1["_created"] = self._parse_date(timezone, c2['createdDate'])
+            # update contact updated and created date:
+            for c1, c2 in zip(item['contacts'], data['contacts']):
+                c1["_updated"] = self._parse_date(timezone, c2['updatedDate'])
+                c1["_created"] = self._parse_date(timezone, c2['createdDate'])
 
-        if data.get('eventUploadContents'):
-            item['files'] = [{
-                'filename': fi['originalDocument'],
-                'url': fi['downloadLink'],
-                'original_id': fi['eventUploadId'],
-            } for fi in data['eventUploadContents']]
+            if data.get('eventUploadContents'):
+                item['files'] = [{
+                    'filename': fi['originalDocument'],
+                    'url': fi['downloadLink'],
+                    'original_id': fi['eventUploadId'],
+                } for fi in data['eventUploadContents']]
 
-        if data.get('lastUpdateTime'):
-            item['_updated'] = self._parse_date(timezone, data['lastUpdateTime'])
+            if data.get('lastUpdateTime'):
+                item['_updated'] = self._parse_date(timezone, data['lastUpdateTime'])
 
-        for rubric in [data['mainRubric']] + data.get('rubricEvent', []):
-            rubric = self._get_en_item(rubric['rubricTexts'])
-            item['calendars'].append({
-                'is_active': True,
-                'name': rubric['rubricName'],
-                'qcode': rubric['rubricName'].lower()
-            })
-        return item
+            for rubric in [data['mainRubric']] + data.get('rubricEvent', []):
+                rubric = self._get_en_item(rubric['rubricTexts'])
+                item['calendars'].append({
+                    'is_active': True,
+                    'name': rubric['rubricName'],
+                    'qcode': rubric['rubricName'].lower()
+                })
+            return item
+        except KeyError as e:
+            logger.error('Provider %s. Item ID %s. Key %s is not exist',
+                         provider.get('name'), data.get('eventId'), e.args)
 
     def _get_en_item(self, list_items):
         """Return entries have EN language from a list of entries with multiple language
@@ -123,42 +127,43 @@ class BelgaQueueEventsParser(FeedParser):
                 'line': [location['streetNumber'], location['street']],
                 'locality': location['city'],
                 'area': location['province'],
-                'country': self._get_en_item(location['country']['countryTexts']),
+                'country': self._get_en_item(location['country']['countryTexts'])['name'],
                 'postal_code': location['postCode'],
             },
             'original_id': location['venueId'],
         }]
 
     def _parse_contacts(self, contacts):
+        def _get_contact_values(contact, types):
+            return [contact.get(_type) for _type in types if contact.get(_type)]
+
         phone_types = [
-            'phoneGeneral', 'directPhone1', 'directPhone2', 'gsm1', 'gsm2', 'personalPhone', 'personalGsm',
-            'personalMobile', 'semaphone', 'mobile247',
+            'phoneGeneral', 'directPhone1', 'directPhone2', 'gsm1', 'gsm2', 'semaphone',
         ]
-        email_types = ['email', 'personalEmail']
-        address_types = ['professionalAddress', 'personalAddress']
+        phone_confidential_types = ['personalPhone', 'personalGsm']
         return [{
-            'original_id': contact['contactId'],
-            "honorific": "asdf",
-            'first_name': contact['firstName'],
-            'last_name': contact['lastName'],
-            'organisation': contact['company'],
-            'contact_email': [contact.get(email) for email in email_types if contact.get(email)],
+            'original_id': contact.get('contactId'),
+            'honorific': None,
+            'first_name': contact.get('firstName'),
+            'last_name': contact.get('lastName'),
+            'organisation': contact.get('company'),
+            'job_title': contact.get('function'),
+            'contact_email': _get_contact_values(contact, ['email', 'personalEmail']),
             'contact_phone': [{
                 'number': contact.get(phone),
-                'public': False,
-                'usage': 'Confidential',
-            } for phone in phone_types if contact.get(phone)],
-            "website": contact.get('url'),
-            "twitter": contact.get('twitter'),
-            "facebook": contact.get('facebook'),
-            "contact_address": [contact.get(address) for address in address_types if contact.get(address)],
-            "notes": contact.get('information'),
+                'usage': 'Confidential' if phone in phone_confidential_types else 'Business',
+                'public': phone not in phone_confidential_types,
+            } for phone in phone_types + phone_confidential_types if contact.get(phone)],
+            'mobile': _get_contact_values(contact, ['personalMobile', 'mobile247']),
+            'website': contact.get('url'),
+            'twitter': contact.get('twitter'),
+            'facebook': contact.get('facebook'),
+            'fax': contact.get('fax'),
+            "contact_address": _get_contact_values(contact, ['professionalAddress', 'personalAddress']),
+            'notes': contact.get('information'),
             "is_active": True,
-            "public": contact['belgaPublic'] == 'Y',
+            'public': contact.get('belgaPublic') == 'Y',
         } for contact in contacts]
-
-    def parse_recurence(self, item):
-        return [item]
 
 
 register_feed_parser(BelgaQueueEventsParser.NAME, BelgaQueueEventsParser())
---
+++ server/tests/io/feed_parsers/belga_queue_events_test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013 - 2018 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+import json
+import os
+from datetime import datetime, timezone
+
+from belga.io.feed_parsers.belga_queue_events import BelgaQueueEventsParser
+from superdesk.tests import TestCase
+from superdesk.utc import local_to_utc
+
+
+class BelgaQueueEventsTest(TestCase):
+    filename = 'queue_events.json'
+
+    def setUp(self):
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        fixture = os.path.normpath(os.path.join(dirname, '../fixtures', self.filename))
+        provider = {'name': 'test'}
+        with open(fixture, 'r') as f:
+            parser = BelgaQueueEventsParser()
+            self.data = json.load(f)['data']
+            self.item = parser.parse(self.data, provider)
+
+    def test_can_parse(self):
+        self.assertTrue(BelgaQueueEventsParser().can_parse(self.data))
+
+    def test_content(self):
+        item = self.item
+        self.assertEqual(item['original_id'], 345678)
+        self.assertEqual(item['name'], 'Belga event')
+        self.assertEqual(item['links'][0], 'https://belga.be')
+        self.assertEqual(
+            item['definition_short'],
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+            "Praesent a erat vel nisl pretium vehicula. Donec vestibulum tempor accumsan.")
+        self.assertEqual(item['versioncreated'], datetime(2019, 6, 30, 4, tzinfo=timezone.utc))
+        self.assertListEqual(item['calendars'], [{
+            'is_active': True,
+            'name': 'NEWS',
+            'qcode': 'news',
+        }, {
+            'is_active': True,
+            'name': 'POLITICS',
+            'qcode': 'politics',
+        }])
+        self.assertDictEqual(item['dates'], {
+            'start': datetime(2019, 6, 30, 22, tzinfo=timezone.utc),
+            'end': datetime(2019, 7, 1, 21, 59, tzinfo=timezone.utc),
+            'tz': 'Europe/Brussels'
+        })
+        self.assertDictEqual(item['files'][0], {
+            'filename': 'image.jpg',
+            'url': 'http://localhost/pressagenda/agenda/event-details-document.action?id=345678&docName=image.jpg',
+            'original_id': 234567,
+
+        })
+        self.assertDictEqual(item['locations'][0], {
+            'name': "ING Contact Centre LLN",
+            'address': {
+                'line': ["6", "traverse d'Esope"],
+                'locality': "Louvain-la-Neuve",
+                'area': "BRABANT WALLON",
+                'country': "BELGIUM",
+                'postal_code': "1348",
+            },
+            'original_id': 250250,
+        })
+        contact = item['contacts'][0]
+        self.assertEqual(contact['original_id'], 316395)
+        self.assertEqual(contact['first_name'], 'Laurent')
+        self.assertEqual(contact['last_name'], 'Dery')
+        self.assertEqual(contact['organisation'], 'Lorem Ipsum')
+        self.assertListEqual(contact['contact_email'], ['Laurent@gmail.com', 'DLaurent@gmail.com'])
+        self.assertListEqual(contact['contact_phone'], [{
+            'number': '+32 2 226 13 51',
+            'usage': 'Business',
+            'public': True,
+        }, {
+            'number': '+32 495 50 25 94',
+            'usage': 'Business',
+            'public': True,
+        }, {
+            'number': '123456789',
+            'usage': 'Confidential',
+            'public': False,
+        }])
